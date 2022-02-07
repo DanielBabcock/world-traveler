72085eadc83534936891887060e9731f9ac56b99
Daniel Babcock <babcock.daniel.b@gmail.com>
2022-02-07 14:14:35 -0500

    update

--- js/wt_cards__travel.js
+++ js/wt_cards__travel.js
@@ -12,29 +12,42 @@ $.ajax({
     console.log("error", error);
   });
 
+const htmlCard = (item , initialClass) => {
+  const initialDiv = initialClass ? `<div class=${initialClass}>` : '<div>'
+  return `${initialDiv}
+            <div class="row">
+              <div class="col">
+                  <img src="${item.image}" alt="image">
+              </div>
+              <div class="col">
+                  <p class="wt-card--travel-body-cta">
+                      ${item.cta}
+                  </p>
+                  <p class="wt-card--travel-location">
+                      ${item.location}
+                  </p>
+                  <p class="wt-card--travel-date">
+                      ${item.date}
+                  </p>
+                  <p class="wt-card--travel-text">
+                      ${item.text}
+                  </p>
+              </div>
+          </div>
+        </div>`
+}
+
+
 function techs(list) {
   let data = list.cards;
+  let doc;
   data.forEach(function (item) {
-    document.getElementById("wt_cards__travel").innerHTML +=
-      `<div class="flex-row">
-          <div class="wt-card--flex-left flex-column">
-              <img src="${item.image}" alt="image">
-          </div>
-          <div class="wt-card--flex-right flex-column">
-              <p class="wt-card--travel-cta">
-                  ${item.cta}
-              </p>
-              <p class="wt-card--travel-location">
-                  ${item.location}
-              </p>
-              <p class="wt-card--travel-date">
-                  ${item.date}
-              </p>
-              <p class="wt-card--travel-text">
-                  ${item.text}
-              </p>
-          </div>
-      </div>`;
-
+    if( item.id === 0 ){
+      doc = document.querySelector(".wt_cards__travel_large") 
+      doc.innerHTML = htmlCard(item)
+    } else {
+      doc = document.querySelector(".wt_cards__travel_small")
+      doc.innerHTML += htmlCard(item, 'col')
+    }
   });
 };
